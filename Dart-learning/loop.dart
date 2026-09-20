4d5d540c2e3fc7542c81225e8811d9697827bdd8
Maryam <maryamabdulrehman303@gmail.com>
2026-09-21 00:00:04 +0500

    oop

---
+++ Dart-learning/loop.dart
@@ -0,0 +1,42 @@
+// Q:26
+// void main() {
+//   for (int i = 1; i <= 10; i++) {
+//     print(i);
+//   }
+// }
+
+// Q:27
+// void main() {
+//   for (int i = 10; i >= 1; i--) {
+//     print(i);
+//   }
+// }
+// Q:28
+// void main() {
+//   for (int i = 1; i <= 50; i++) {
+//     if (i % 2 == 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+// Q:29
+// void main() {
+//   for (int i = 1; i <= 50; i++) {
+//     if (i % 2 != 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+// Q:30
+
+void main() {
+  int sum = 0;
+
+  for (int i = 1; i <= 100; i++) {
+    sum = sum + i;
+  }
+
+  print("Sum = $sum");
+}
---
+++ Dart-learning/switch.dart
@@ -0,0 +1,267 @@
+// Q16:
+// void main(){
+//   int day = 3;
+
+//   switch(day){
+//     case 1:
+//       print("Monday");
+//       break;
+//     case 2:
+//       print("Tuesday");
+//       break;
+//     case 3:
+//       print("Wednesday");
+//       break;
+//     case 4:
+//       print("Thursday");
+//       break;
+//     case 5:
+//       print("Friday");
+//       break;
+//     case 6:
+//       print("Saturday");
+//       break;
+//     case 7:
+//       print("Sunday");
+//       break;
+//     default:
+//       print("Invalid day"); }}
+
+
+// Q:17
+
+// void main() {
+//   int month = 5;
+
+//   switch (month) {
+//     case 1:
+//       print("January");
+//       break;
+//     case 2:
+//       print("February");
+//       break;
+//     case 3:
+//       print("March");
+//       break;
+//     case 4:
+//       print("April");
+//       break;
+//     case 5:
+//       print("May");
+//       break;
+//     case 6:
+//       print("June");
+//       break;
+//     case 7:
+//       print("July");
+//       break;
+//     case 8:
+//       print("August");
+//       break;
+//     case 9:
+//       print("September");
+//       break;
+//     case 10:
+//       print("October");
+//       break;
+//     case 11:
+//       print("November");
+//       break;
+//     case 12:
+//       print("December");
+//       break;
+//     default:
+//       print("Invalid month");
+//   }
+// }
+
+
+  // Q:18
+//   void main() {
+//   String signal = "R";
+
+//   switch (signal) {
+//     case "R":
+//       print("Stop");
+//       break;
+//     case "Y":
+//       print("Get Ready");
+//       break;
+//     case "G":
+//       print("Go");
+//       break;
+//     default:
+//       print("Invalid signal");
+//   }
+// }
+
+// Q:19
+
+// void main() {
+//   double num1 = 20;
+//   double num2 = 5;
+//   String operator = "+";
+
+//   switch (operator){
+//     case "+":
+//       print(num1 + num2);
+//       break;
+//     case "-":
+//       print(num1 - num2);
+//       break;
+//     case "*":
+//       print(num1 * num2);
+//       break;
+//     case "/":
+//       print(num1 / num2);
+//       break;
+//     default:
+//       print("Invalid operator");
+//   }
+// }
+
+
+// Q:20
+
+// void main() {
+//   int season = 2;
+
+//   switch (season) {
+//     case 1:
+//       print("Spring");
+//       break;
+//     case 2:
+//       print("Summer");
+//       break;
+//     case 3:
+//       print("Autumn");
+//       break;
+//     case 4:
+//       print("Winter");
+//       break;
+//     default:
+//       print("Invalid season");
+//   }
+// }
+
+
+// Q:21
+
+// void main() {
+//   String grade = "A";
+//   switch(grade) {
+//     case "A":
+//       print("Excellent");
+//       break;
+//     case "B":
+//       print("Very Good");
+//       break;
+//     case "C":
+//       print("Good");
+//       break;
+//     case "D":
+//       print("Needs Improvement");
+//       break;
+//     case "F":
+//       print("Fail");
+//       break;
+//     default:
+//       print("Invalid grade");
+//   }
+// }
+
+
+// Q:22
+
+// void main() {
+//   int choice = 2;
+
+//   switch (choice) {
+//     case 1:
+//       print("Pizza");
+//       break;
+//     case 2:
+//       print("Burger");
+//       break;
+//     case 3:
+//       print("Pasta");
+//       break;
+//     case 4:
+//       print("Sandwich");
+//       break;
+//     case 5:
+//       print("Fries");
+//       break;
+//     default:
+//       print("Invalid choice");
+//   }
+// }
+
+// Q:23
+
+// void main() {
+//   int role = 3;
+//   switch (role){
+//     case 1:
+//       print("Admin");
+//       break;
+//     case 2:
+//       print("Teacher");
+//       break;
+//     case 3:
+//       print("Student");
+//       break;
+//     default:
+//       print("Invalid role");
+//   }
+// }
+
+// Q:24
+// void main() {
+//   String day = "S";
+//   switch (day) {
+//     case "M":
+//       print("Weekday");
+//       break;
+//     case "T":
+//       print("Weekday");
+//       break;
+//     case "W":
+//       print("Weekday");
+//       break;
+//     case "Th":
+//       print("Weekday");
+//       break;
+//     case "F":
+//       print("Weekday");
+//       break;
+//     case "S":
+//       print("Weekend");
+//       break;
+//     case "Su":
+//       print("Weekend");
+//       break;
+//     default:
+//       print("Invalid day"); }}
+
+      // Q:25
+      void main() {
+  int choice = 3;
+
+  switch (choice) {
+    case 1:
+      print("Check Balance");
+      break;
+    case 2:
+      print("Deposit");
+      break;
+    case 3:
+      print("Withdraw");
+      break;
+    case 4:
+      print("Exit");
+      break;
+    default:
+      print("Invalid choice");
+  }
+}
